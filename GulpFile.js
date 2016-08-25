0d08fa0b9e9b8a2137bddcdcacd0c3dbe68bc9ef
AndrewLang <nnlyx@hotmail.com>
2016-08-25 16:08:18 -0600

    use gulp clean, compile, merge and output

--- GulpFile.js
+++ GulpFile.js
@@ -1,35 +1,50 @@
 'use strict';
 
-var gulp    = require('gulp');
-var sass    = require('gulp-sass');
-var minify  = require('gulp-minify-css');
-var merge   = require('merge2');
-var ts      = require('gulp-typescript');
-var rename  = require('gulp-rename');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minify = require('gulp-minify-css');
+var merge = require('merge2');
+var concat = require('gulp-concat');
+var ts = require('gulp-typescript');
+var rename = require('gulp-rename');
 var sequence = require('run-sequence');
-var shell    = require('gulp-shell');
+var shell = require('gulp-shell');
+var uglify = require('gulp-uglify');
+//var gulpIf   = require('gulp-if');
+var del = require('del');
+
+gulp.task('clean', function () {
+  return del.sync('dist/*.*');
+});
 
 gulp.task('scripts', function () {
   var folder = 'src/app/';
   var tsProject = ts.createProject('tsconfig.json');
-  var result = tsProject.src()  
-    .pipe(ts(tsProject))
-    .pipe(gulp.dest(folder));
+  var dist = 'dist';
+  return tsProject.src()
+    .pipe(ts(tsProject))        // compile with ts configuration
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(dist));
 });
 
 
 gulp.task('sass', function () {
   var folder = 'src/assets/styles/';
-  gulp.src( folder + '*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(minify())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest(folder));
+  var dist = 'dist';
+  return merge(
+    gulp.src(folder + '*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(minify())
+      .pipe(rename({ suffix: '.min' }))
+  )
+    .pipe(concat('app.css'))
+    .pipe(gulp.dest(dist));
 });
 
-gulp.task('electron', function(){
+gulp.task('electron', function () {
   return gulp.src('/')
-      .pipe(shell('electron .'));
+    .pipe(shell('electron .'));
 });
 
 gulp.task('sass:watch', function () {
@@ -37,8 +52,8 @@ gulp.task('sass:watch', function () {
   gulp.watch('*.ts', ['scripts'])
 });
 
-gulp.task('start', function(callback){
-  sequence( 'scripts', "sass","electron", callback );
+gulp.task('start', function (callback) {
+  sequence('clean', 'scripts', "sass", "electron", callback);
 });
 
 // backup of npm
