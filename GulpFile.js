77dd64c4f42d9f556068711b0f5b156ad7d42d9f
AndrewLang <Andy@LangYuXins-MacBook.local>
2016-08-24 21:32:56 -0600

    add gulp task

--- GulpFile.js
+++ GulpFile.js
@@ -2,6 +2,17 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var merge = require('merge2');
+var ts = require('gulp-typescript');
+
+gulp.task('scripts', function () {
+  var result = gulp.src('*.ts')
+    .pipe(ts({
+      declarationFiles: true,
+      noExternalResolve:true,
+      noImplicitAny:true
+    }));
+});
 
 gulp.task('sass', function () {
   gulp.src('*.scss')
@@ -11,4 +22,5 @@ gulp.task('sass', function () {
 
 gulp.task('sass:watch', function () {
   gulp.watch('*.scss', ['sass']);
+  gulp.watch('*.ts', ['scripts'])
 });
